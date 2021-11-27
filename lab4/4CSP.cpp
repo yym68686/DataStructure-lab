864717a063d4dcbbbefa5f5f27d5fc98e1b4ae27
yym68686 <yym68686@outlook.com>
2021-11-27 15:13:30 +0800

    简化 lab4 第四题

--- lab4/4CSP.cpp
+++ lab4/4CSP.cpp
@@ -1,16 +1,15 @@
 #include <stdio.h>
-int n, k[111], viste[22], num, summ = 0;
+int n, k, viste[22], num, summ = 0;
 int main(){
     freopen("4CSP", "r", stdin);
     scanf("%d", &n);
-    while (scanf("%d", &k[num]) != EOF) num++;
-    for (int i = 0; i < num; i++)
+    while (scanf("%d", &k) != EOF)
     for (int j = 0; j < 20; j++)
-        if (5 - viste[j] >= k[i]){
-            for (int kk = 1; kk <= k[i]; kk++)
+        if (5 - viste[j] >= k){
+            for (int kk = 1; kk <= k; kk++)
                 printf("%d ", 5 * j + kk + viste[j]);
             puts("");
-            viste[j] = k[i];
+            viste[j] = k;
             break;
         }
 }
