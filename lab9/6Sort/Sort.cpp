f4cd4f1f6dbf5dd1d61600e68ffbfe17a6191cdd
yym68686 <yym68686@outlook.com>
2022-01-05 02:40:01 +0800

    6Sort 完成基数排序

--- lab9/6Sort/Sort.cpp
+++ lab9/6Sort/Sort.cpp
@@ -122,8 +122,62 @@ void merge(int a[], int s, int e, int b[])
     }
 }
 
+void RadixSort(){
+
+}
+
+int maxbit(int data[], int n) //辅助函数，求数据的最大位数
+{
+    int maxData = data[0];              ///< 最大数
+    /// 先求出最大数，再求其位数，这样有原先依次每个数判断其位数，稍微优化点。
+    for (int i = 1; i < n; ++i)
+    {
+        if (maxData < data[i])
+            maxData = data[i];
+    }
+    int d = 1;
+    int p = 10;
+    while (maxData >= p)
+    {
+        maxData /= 10;
+        ++d;
+    }
+    return d;
+}
+void RadixSort(int data[], int n) //基数排序
+{
+    int d = maxbit(data, n);
+    int *tmp = new int[n];
+    int *count = new int[10]; //计数器
+    int i, j, k;
+    int radix = 1;
+    for(i = 1; i <= d; i++) //进行d次排序
+    {
+        for(j = 0; j < 10; j++)
+            count[j] = 0; //每次分配前清空计数器
+        for(j = 0; j < n; j++)
+        {
+            k = (data[j] / radix) % 10; //统计每个桶中的记录数
+            count[k]++;
+        }
+        for(j = 1; j < 10; j++)
+            count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
+        for(j = n - 1; j >= 0; j--) //将所有桶中记录依次收集到tmp中
+        {
+            k = (data[j] / radix) % 10;
+            tmp[count[k] - 1] = data[j];
+            count[k]--;
+        }
+        for(j = 0; j < n; j++) //将临时数组的内容复制到data中
+            data[j] = tmp[j];
+        radix = radix * 10;
+    }
+    delete []tmp;
+    delete []count;
+}
+
 int main(){
-    cout << setw(2 * w + 4) << "Insert" << setw(w + 1) << "Shell" << setw(w + 1) << "Bubble" << setw(w + 1) << "Select" << setw(w + 1) << "Heap" << setw(w + 1) << "Merge" << setw(w + 1) << "Insert" << setw(w + 1) << "Insert" << endl;
+    cout << setw(2 * w + 4) << "Insert" << setw(w + 1) << "Shell" << setw(w + 1) << "Bubble" << setw(w + 1) << "Select" << setw(w + 1) << "Heap" << setw(w + 1) << "Merge" << setw(w + 1) << "Radix" << setw(w + 1) << "Insert" << endl;
     while (inde != 11){
         n = 0;
         fstream ReadFile(to_string(inde) + "Sample");
@@ -170,6 +224,12 @@ int main(){
         for (int i = 0; i < n; i++) arr[i] = num[i];
         starttime = clock();
         merge(arr, 0, n - 1, mergearr);
+        endtime = clock();
+        cout << setw(w) << fixed << setprecision(3) << double(endtime - starttime) / CLOCKS_PER_SEC << "s";
+
+        for (int i = 0; i < n; i++) arr[i] = num[i];
+        starttime = clock();
+        RadixSort(arr, n);
         endtime = clock();
         cout << setw(w) << fixed << setprecision(3) << double(endtime - starttime) / CLOCKS_PER_SEC << "s" << endl;
 
